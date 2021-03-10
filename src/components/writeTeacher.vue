2fdde5c70f07101f98ead026c53dde6658b35813
MingYang-Yin <64779177+MingYang-Yin@users.noreply.github.com>
2021-03-10 05:35:55 -0800

    Update writeTeacher.vue

--- src/components/writeTeacher.vue
+++ src/components/writeTeacher.vue
@@ -1,28 +1,40 @@
 <template>
-  <div>
+  <div id="firstDiv" >
     <p>
       <font size="5" face="Verdana">
         请输入教师信息
       </font>
     </p>
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" align="center">
-      <el-form-item label="姓名" prop="name" required>
-        <el-input v-model="ruleForm.name"></el-input>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" style="text-align: center;">
+      <el-form-item label="姓名" prop="name" required style="width: 300px;margin:0 auto;">
+        <el-col  style="text-align: center">
+          <el-input v-model="ruleForm.name"></el-input>
+        </el-col>
       </el-form-item>
+      <br>
 
-      <el-form-item label="学工号" prop="id" align="center" required>
-        <el-input v-model="ruleForm.id"></el-input>
+      <el-form-item label="学工号" prop="id" style="width: 300px;margin:0 auto;" required>
+        <el-col >
+          <el-input v-model="ruleForm.id"></el-input>
+        </el-col>
       </el-form-item>
+      <br>
 
-      <el-form-item label="学院" prop="institute" align="center" required>
-        <el-input v-model="ruleForm.institute"></el-input>
+      <el-form-item label="学院" prop="institute" style="width: 300px;margin:0 auto;" required>
+        <el-col>
+          <el-input v-model="ruleForm.institute"></el-input>
+        </el-col>
       </el-form-item>
+      <br>
 
-      <el-form-item label="联系方式" prop="phone" align="center" required>
-        <el-input v-model="ruleForm.phone"></el-input>
+      <el-form-item label="联系方式" prop="phone" style="width: 300px;margin:0 auto;" required>
+        <el-col>
+          <el-input v-model="ruleForm.phone"></el-input>
+        </el-col>
       </el-form-item>
+      <br>
 
-      <el-form-item align="center">
+      <el-form-item style="width: 300px;margin:0 auto;">
         <el-button type="primary" @click="submitForm('ruleForm')">添加</el-button>
         <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
@@ -66,20 +78,20 @@
         this.$refs[formName].validate((valid) => {
           if (valid) {
             var submit = {  //JSON数据		名称-值对   需要根据后端修改
-              "campus": this.ruleForm.name, //姓名
-              "building": this.ruleForm.id, //学工号
-              "room": this.ruleForm.institute, //学院
-              "equipmentNum": this.ruleForm.phone, //电话号
+              "name": this.ruleForm.name, //姓名 name, aid, academic, tel
+              "aid": this.ruleForm.id, //学工号
+              "academic": this.ruleForm.institute, //学院
+              "tel": this.ruleForm.phone, //电话号
             }
             this.$axios({
               method: 'post',
-              url: 'http://localhost:8080/computerRoom/addRoom',
+              url: 'http://localhost:8080/user/ManualImport',
               contentType: 'application/json; charset=UTF-8',// 解决415错误
               headers:{'Content-Type':'application/json;charset=UTF-8'},
               dataType: 'json',
               data: JSON.stringify(submit),
             }).then(res => {
-              console.log(this.formInline.campus)
+              console.log("ok")
               if(res.data === 1){
                 alert("添加成功")
               }
@@ -105,5 +117,14 @@
 </script>
 
 <style scoped>
-
+  #firstDiv{
+    width: 100px;
+    margin:0px auto;
+    text-align:center;
+    border:5px solid #000
+  }
+  .el-form-item{
+    text-align: center;
+  }
+/*  div{ border:3px solid #000}*/
 </style>
