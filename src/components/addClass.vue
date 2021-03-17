52b8e2997ecda1543c60a26704ac4b65c8c1e5c7
MingYang-Yin <64779177+MingYang-Yin@users.noreply.github.com>
2021-03-17 05:40:04 -0800

    Add files via upload

---
+++ src/components/addClass.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-menu :default-active=1 class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index=1 @click="goTo('/menu/addClass')">手动添加</el-menu-item>
+      <el-menu-item index=2 @click="goTo('/menu/addClass/importTeacher')">导入文件</el-menu-item>
+    </el-menu>
+
+    <div><!--放置页面主体内容-->
+      <router-view id="homepage_view"></router-view>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'addClass',
+    methods:{
+      goTo (path) {
+        this.$router.push(path);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
